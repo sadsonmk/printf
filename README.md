312a44599641a14ff702e03762cc52fb2e98fe30
sadson mk <sadsonmkumira@yahoo.com>
2023-04-22 23:10:10 -0700

    Adding the readme and main.h

---
+++ README.md
@@ -0,0 +1,2 @@
+This is the readme file for the project 0x11. C - printf
+
---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * _printf - produces output according to a format.
+ * @format: is a character string
+ * Return: the number of characters printed 
+ * (excluding the null byte used to end output to strings)
+ *
+ */
+
+int _printf(const char *format, ...);
+
+#endif /* main.h */
