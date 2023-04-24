5f62b5ed5fbf2e999c584f846d9a7206c4614058
sadson mk <sadsonmkumira@yahoo.com>
2023-04-24 06:01:19 -0700

    Adding files

--- _printf.c
+++ _printf.c
@@ -1,31 +1,50 @@
 #include <stdio.h>
 #include "main.h"
-
-
 /**
  * _printf - produces output according to a format.
  * @format: is a character string
  * Return: the number of characters printed
  * (excluding the null byte used to end output to strings)
- *
  */
-
 int _printf(const char *format, ...)
 {
 	va_list arg;
-
-	int counter = 0;
-
+	int flag = 0, counter = 0;
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(arg, format);
+	while (*format != '\0')
+	{
+		if (flag == 0)
+		{
+			if (*format == '%')
+				flag = 1;
+			_putchar(*format);
+		}
+		else if (flag == 1)
+		{
+			switch (*format)
+			{
+				case 'c':
+					_putchar(va_arg(arg, int));
+					counter++;
+					break;
+				case 's':
+					{
+					char *str = va_arg(arg, char *);
 
-	_vprintf(format, arg);
-	counter++;
-
-	va_end(arg);
-
+					while (*str)
+					{
+						_putchar(*str++);
+						counter++;
+					}
+					break;
+					}
+			}
+		}
+		flag = 0, format++;
+	}
+	counter++, va_end(arg);
 	return (counter);
 }
